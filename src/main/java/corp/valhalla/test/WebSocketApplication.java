d0f96e1672315071ad9477efb70a602378bfd209
Brian <brianmr31@gmail.com>
2020-03-06 07:09:41 +0700

    skss

--- src/main/java/corp/valhalla/test/WebSocketApplication.java
+++ src/main/java/corp/valhalla/test/WebSocketApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebSocketApplication {
 
+	int check;
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebSocketApplication.class, args);
 	}
